0610e5e88751ea3a3940f6a1e28261dc183f1a33
ericyounger <ericy@stud.ntnu.no>
2020-08-26 13:23:06 +0200

    finished exercise2

--- exercise2/task2/app/src/main/java/com/example/task2/randomActivity.java
+++ exercise2/task2/app/src/main/java/com/example/task2/randomActivity.java
@@ -1,40 +1,37 @@
-package com.example.numbers;
-
-
+package com.example.task2;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.Random;
 
+public class randomActivity extends AppCompatActivity {
 
-public class otherActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         Random rnd = new Random();
 
-        //Toast.makeText(this, number, Toast.LENGTH_LONG).show();
-
         int upperLimit = getIntent().getIntExtra("upperLimit", 1);
+        Log.i("Intent","Mottok intent med upper limit");
 
-        int number = rnd.nextInt(upperLimit);
+
+        int array[] = {rnd.nextInt(upperLimit), rnd.nextInt(upperLimit)};
 
         Intent intent = new Intent();
-        intent.putExtra("number", number);
+        intent.putExtra("numbers", array);
         setResult(RESULT_OK, intent);
         finish();
 
     }
-
-
-
 
 }
